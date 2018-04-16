2fb0ac6ec4e599b3c8ad4b7776fc2334fa295417
xiaoshen.wang <xiaoshen.wang@msxf.com>
2018-04-16 10:55:25 +0800

    configclient.properties

--- config-server/src/main/java/com/wxs/cloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/wxs/cloud/ConfigServerApplication.java
@@ -1,13 +1,26 @@
 package com.wxs.cloud;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Created by xiaoshen.wang on 2018/4/12
  * 配置中心：服务端
+ * http请求地址和资源文件映射如下:
+ * /{application}/{profile}[/{label}]
+ * /{application}-{profile}.yml
+ * /{label}/{application}-{profile}.yml
+ * /{application}-{profile}.properties
+ * /{label}/{application}-{profile}.properties
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApplication {
+    public static void main(String[] args){
+        new SpringApplicationBuilder(ConfigServerApplication.class).bannerMode(Banner.Mode.OFF).web(true).run(args);
+    }
 }
